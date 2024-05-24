b651eb60ceb4b230ae68332e5803d67f287edd0f
nvanschaack <nellvanschaack@gmail.com>
2024-05-24 15:55:07 -0500

    intial commit

---
+++ models/Thought.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema()
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require('mongoose');
+
+//Schema to create a User model
+const userSchema = new mongoose.Schema()
+
+
+module.exports = User;
